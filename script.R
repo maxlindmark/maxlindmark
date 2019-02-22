a688007f33424fa7232adbd4c354fa5dcf04e3a3
Max Lindmark <maxo.lindmark@gmail.com>
2019-02-22 16:22:51 +0100

    fix

--- script.R
+++ script.R
@@ -8,6 +8,10 @@
 # https://notes.peter-baumgartner.net/tutorial/blogdown-tutorial-part-3/
 
 
+# initial commmiting issues:
+# https://support.rstudio.com/hc/en-us/community/posts/200656917-Cannot-tick-the-stage-checkbox-of-modified-files-in-the-git-pane
+# https://stackoverflow.com/questions/38004148/another-git-process-seems-to-be-running-in-this-repository
+
 # .libPaths()
 # .libPaths("C:/Program Files/R/R-3.5.0/library")
 # .libPaths()
