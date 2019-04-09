b62ac9b0992a2ec1368ac617462227f36e7cf967
Max Lindmark <maxo.lindmark@gmail.com>
2019-04-09 13:54:45 +0200

    testing weights

--- script.R
+++ script.R
@@ -1,6 +1,6 @@
 #remotes::install_github('rstudio/blogdown')
 
-#blogdown::new_site(theme = "gcushen/hugo-academic", theme_example = TRUE)
+# blogdown::new_site(theme = "gcushen/hugo-academic", theme_example = TRUE)
 
 # blogdown::serve_site()
 
@@ -10,15 +10,22 @@
 # and
 # https://notes.peter-baumgartner.net/tutorial/blogdown-tutorial-part-3/
 # https://www.molecularecologist.com/2013/11/using-github-with-r-and-rstudio/
+# https://djnavarro.net/post/2018-04-27-starting-blogdown/
+
 
 # initial commmiting issues:
 # https://support.rstudio.com/hc/en-us/community/posts/200656917-Cannot-tick-the-stage-checkbox-of-modified-files-in-the-git-pane
+
+# git add . # if you can commit
+
+# rm -f .git/index.lock # if another process is runnig so you can't commit
+
 # https://stackoverflow.com/questions/38004148/another-git-process-seems-to-be-running-in-this-repository
 
 # .libPaths()
 # .libPaths("C:/Program Files/R/R-3.5.0/library")
 # .libPaths()
-# 
+
 
 # launch: if it won't deploy, read the status and specify right version of hugo!
 
@@ -26,6 +33,11 @@
 #-- Add research projects?
 
 
+## Icons for website: 
+# Social/Academic Networking
+# For available icons, see: https://sourcethemes.com/academic/docs/widgets/#icons
+#   For an email link, use "fas" icon pack, "envelope" icon, and a link in the
+#   form "mailto:your-email@example.com" or "#contact" for contact widget.
 
 ## Approach for WK:
 # - get git
